7eb137d3da1aae7d192ec2492dcd61b61967dd85
taahmed045-stack <taahmed045@gmail.com>
2026-07-04 09:06:14 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
+
+  socket.on("dice", (data) => {
+    io.emit("dice", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
